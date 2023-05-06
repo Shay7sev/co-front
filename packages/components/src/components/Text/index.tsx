23dc7c56e980b96dfc65e06feb375dfd2561610a
shay <shay7sev@gmail.com>
2023-05-06 16:22:49 +0800

    feat(global): common packages

---
+++ packages/components/src/components/Text/index.tsx
@@ -0,0 +1,9 @@
+import { Typography } from 'antd'
+import { TextProps } from 'antd/es/typography/Text'
+import { memo } from 'react'
+
+const Text = ({ children, ...props }: TextProps) => (
+  <Typography.Text {...props}>{children}</Typography.Text>
+)
+
+export default memo(Text)
---
+++ packages/utils/module/contans.ts
@@ -0,0 +1,8 @@
+export const TOKEN = 'TOKEN'
+
+export const ADMIN = 'admin'
+
+export const Settings = {
+  title: 'co-front',
+  logo: './logo.jpg',
+} as const
