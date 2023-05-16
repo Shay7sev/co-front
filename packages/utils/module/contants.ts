9cf34a200096f44adcde8a44474de98e6c4fe2c0
shay <shay7sev@gmail.com>
2023-05-16 21:05:25 +0800

    fix(global): setting fix

--- packages/utils/module/contants.ts
+++ packages/utils/module/contants.ts
@@ -2,6 +2,11 @@ export const TOKEN = 'TOKEN'
 
 export const ADMIN = 'admin'
 
+// * 登录页地址（默认）
+export const LOGIN_URL = '/login'
+
+export const ROUTER_WHITE_LIST = ['/login']
+
 export const Settings = {
   title: 'co-front',
   logo: './vite.svg',
