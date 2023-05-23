9a05990fa129b3a3505e8e9f4bce92f425c9de15
shay <shay7sev@gmail.com>
2023-05-23 15:41:44 +0800

    fix(packages): eslint rule

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -3,13 +3,23 @@ module.exports = {
     node: true,
   },
   parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020,
   },
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
     // eslint (http://eslint.cn/docs/rules)
     'no-var': 'error', // 要求使用 let 或 const 而不是 var
     eqeqeq: [2, 'allow-null'], // 使用 === 替代 ==
