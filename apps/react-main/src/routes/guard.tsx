d7d30f0f8073164f63028e81ad54dcc450b36a7a
shay <shay7sev@gmail.com>
2023-05-28 17:28:14 +0800

    fix(react-main): react drop guard

--- apps/react-main/src/routes/guard.tsx
+++ apps/react-main/src/routes/guard.tsx
@@ -1,35 +1,37 @@
 import { useEffect } from 'react'
-import { useLocation, useRoutes, Location } from 'react-router-dom'
-import { useAppDispatch, useAppSelector } from 'hooks'
-import { Settings, ADMIN, ROUTER_WHITE_LIST, TOKEN, removeStorage } from 'utils'
-import { setMenu, setUserToken, store } from 'store'
+import { useRoutes } from 'react-router-dom'
+import { useAppDispatch, useAppSelector, useLocationListen } from 'hooks'
+import { Settings, ADMIN } from 'utils'
+import { setMenu } from 'store'
 import { MenuData } from '@/common/mock'
 import { defaultRoutes, filepathToElement } from './index'
 import { cloneDeep } from 'lodash'
 
 //全局路由守卫
-function guard(location: Location, token: string) {
-  // 1.动态设置标题
-  const { pathname } = location
-  document.title = `${Settings.title}: ${pathname.replace('/', '')}`
+// function guard(location: Location, token: string) {
+//   // 1.动态设置标题
+//   const { pathname } = location
+//   document.title = `${Settings.title}: ${pathname.replace('/', '')}`
 
-  // 2.判断访问页面是否在路由白名单地址中，如果存在直接放行 layout路由因为permission组件不通行
-  if (ROUTER_WHITE_LIST.includes(pathname)) return true
+//   // 2.判断访问页面是否在路由白名单地址中，如果存在直接放行 layout路由因为permission组件不通行
+//   if (ROUTER_WHITE_LIST.includes(pathname)) return true
 
-  // 3.判断是否有 Token
-  if (!token) {
-    removeStorage(TOKEN)
-    store.dispatch(setUserToken(''))
-  }
+//   // 3.判断是否有 Token
+//   if (!token) {
+//     removeStorage(TOKEN)
+//     store.dispatch(setUserToken(''))
+//   }
 
-  return true
-}
+//   return true
+// }
 
 export const RouterGurad = () => {
   const {
     user: { token, menu },
   } = useAppSelector((state) => state)
-  const location = useLocation()
+
+  // const location = useLocation()
+
   const dispatch = useAppDispatch()
 
   const cloneDefaultRoutes = cloneDeep(defaultRoutes)
@@ -37,6 +39,10 @@ export const RouterGurad = () => {
     ...filepathToElement(menu),
     ...cloneDefaultRoutes[0].children!,
   ]
+
+  useLocationListen((r) => {
+    document.title = `${Settings.title}: ${r.pathname.replace('/', '')}`
+  })
 
   useEffect(() => {
     /**
@@ -49,9 +55,9 @@ export const RouterGurad = () => {
     }
   }, [token])
 
-  useEffect(() => {
-    guard(location, token)
-  }, [location])
+  // useEffect(() => {
+  //   guard(location, token)
+  // }, [location])
 
   const Route = useRoutes(cloneDefaultRoutes)
   return Route
